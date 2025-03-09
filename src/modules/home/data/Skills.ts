ce88f25d7ebde37aae796f57171cb2379a7499d4
JuanZuniga79 <cd.juanzuniga@gmail.com>
2025-03-09 00:50:32 -0500

    skills added

--- src/modules/home/data/Skills.ts
+++ src/modules/home/data/Skills.ts
@@ -4,33 +4,33 @@ export default interface ISkills {
 }
 
 export const frontend: ISkills[] = [
-    {name: "next", level: 10},
-    {name: "react", level: 8},
-    {name: "astro", level: 10},
-    {name: "angular", level: 6},
-    {name: "remix", level: 2},
+    { name: "next", level: 10 },
+    { name: "react", level: 8 },
+    { name: "astro", level: 10 },
+    { name: "angular", level: 6 },
+    { name: "remix", level: 2 },
 ]
 
 export const backend: ISkills[] = [
-    {name: "spring boot", level: 10},
-    {name: "asp.net core", level: 6},
-    {name: "nest", level: 6},
-    {name: "fiber", level: 2},
+    { name: "spring boot", level: 10 },
+    { name: "asp.net core", level: 6 },
+    { name: "nest", level: 6 },
+    { name: "fiber", level: 4 },
 ]
 
 export const databases: ISkills[] = [
-    {name: "postgres", level: 8},
-    {name: "sqlite", level: 8},
-    {name: "sql server", level: 6},
-    {name: "mongo", level: 4},
-    {name: "redis", level: 4},
+    { name: "postgres", level: 8 },
+    { name: "sqlite", level: 8 },
+    { name: "sql server", level: 7 },
+    { name: "mongo", level: 4 },
+    { name: "redis", level: 4 },
 ]
 
 export const languages: ISkills[] = [
-    {name: "typescript", level: 10},
-    {name: "javascript", level: 6},
-    {name: "java", level: 8},
-    {name: "csharp", level: 8},
-    {name: "kotlin", level: 6},
-    {name: "python", level: 6},
+    { name: "typescript", level: 10 },
+    { name: "javascript", level: 6 },
+    { name: "java", level: 8 },
+    { name: "csharp", level: 8 },
+    { name: "kotlin", level: 6 },
+    { name: "python", level: 6 },
 ]
