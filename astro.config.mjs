cc982b30ce1128fb602aa618b9f3b5cea6f0c423
janz-code <jczp_020401@hotmail.com>
2025-07-11 00:37:21 -0500

    pure astro theme selector done, header almost done and react integration added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 import vercel from "@astrojs/vercel";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -19,15 +21,18 @@ export default defineConfig({
           redirectToDefaultLocale: true,
       }
   },
+
   prefetch: {
       defaultStrategy: "hover",
       prefetchAll: true
   },
+
   trailingSlash: "ignore",
   compressHTML: false,
   devToolbar: {enabled: false},
   image: {responsiveStyles: true},
   security: {checkOrigin: true},
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [react()]
 });
