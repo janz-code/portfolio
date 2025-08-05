147784d67b9575d8b12a829b3fb377874bdfaeaf
janz-code <jczp_020401@hotmail.com>
2025-08-05 15:50:06 -0500

    projects updated

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,17 +1,18 @@
-import {defineCollection, z} from "astro:content";
-import {glob} from "astro/loaders";
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
 
 const projects = defineCollection({
-    loader: glob({pattern: "**/*.md", base: "./src/commons/content/projects"}),
-    schema: z.object({
-        name: z.string(),
-        picture: z.optional(z.string()),
-        status: z.enum(["done", "in-progress", "planning", "abandoned", "deleted", "featured"]),
-        short_description: z.string(),
-        repo: z.optional(z.string()),
-        live: z.optional( z.string()),
-        stack: z.optional(z.array(z.string())),
-    })
+  loader: glob({ pattern: "**/*.md", base: "./src/commons/content/projects" }),
+  schema: z.object({
+    name: z.string(),
+    picture: z.optional(z.string()),
+    status: z.enum(["done", "in-progress", "planning", "abandoned", "deleted", "featured"]),
+    short_description: z.string(),
+    repo: z.optional(z.string()),
+    live: z.optional(z.string()),
+    stack: z.optional(z.array(z.string())),
+    priority: z.number()
+  })
 })
 
-export const collections = {projects}
+export const collections = { projects }
